ba5b09eb47dffcee27969e76e5054d3e8b2e288e
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-09-14 23:18:42 -0700

    Remove all use of lambda formulas in our grammar
    Cause they're about to die

---
+++ src/edu/stanford/nlp/sempre/thingtalk/CommandFn.java
@@ -0,0 +1,42 @@
+package edu.stanford.nlp.sempre.thingtalk;
+
+import edu.stanford.nlp.sempre.*;
+import fig.basic.LispTree;
+
+public class CommandFn extends SemanticFn {
+  private String commandType;
+  private String secondArg;
+  private boolean withDevice;
+
+  @Override
+  public void init(LispTree tree) {
+    super.init(tree);
+    commandType = tree.child(1).value;
+    if (tree.children.size() > 2) {
+      secondArg = tree.child(2).value;
+      withDevice = tree.child(2).value.equals("withDevice");
+    } else {
+      secondArg = "generic";
+      withDevice = false;
+    }
+  }
+
+  @Override
+  public DerivationStream call(Example ex, Callable c) {
+    return new SingleDerivationStream() {
+      @Override
+      public Derivation createDerivation() {
+        NameValue device;
+        Value cmd;
+        if (withDevice) {
+          device = (NameValue) c.child(0).value;
+          cmd = ThingTalk.cmdForm(new StringValue(commandType), device);
+        } else {
+          cmd = ThingTalk.cmdForm(new StringValue(commandType), new StringValue(secondArg));
+        }
+
+        return new Derivation.Builder().withCallable(c).formula(new ValueFormula<>(cmd)).createDerivation();
+      }
+    };
+  }
+}
