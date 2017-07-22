7d29b609a139a5d12c73ab044814f786856d3ab9
sileixu <silei@cs.stanford.edu>
2017-07-22 14:02:52 +0800

    Add basic support to transfer nl to intermediate json format for time duration Examples - from July 5th to July 7th - since July 5th - last 7 days

--- src/edu/stanford/nlp/sempre/ibase/DurationValue.java
+++ src/edu/stanford/nlp/sempre/ibase/DurationValue.java
@@ -1,9 +1,12 @@
 package edu.stanford.nlp.sempre.ibase;
 
 import edu.stanford.nlp.sempre.DateValue;
+import edu.stanford.nlp.sempre.NumberValue;
 import edu.stanford.nlp.sempre.Value;
 import fig.basic.LispTree;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,22 +16,112 @@ import java.util.Map;
  */
 public class DurationValue extends Value {
 
-    public final DateValue start;
-    public final DateValue end;
+    private final DateValue start;
+    private final DateValue end;
+    private final NumberValue diff;
+    private final Boolean isNow;
 
     public DurationValue(DateValue start, DateValue end) {
         this.start = start;
         this.end = end;
+        this.isNow = false;
+        this.diff = null;
+    }
+
+    public DurationValue(DateValue start) {
+        this.start = start;
+        this.end = DateValue.now();
+        this.isNow = true;
+        this.diff = null;
+    }
+
+    public DurationValue(NumberValue duration) {
+        this.end = DateValue.now();
+        this.diff = duration;
+        this.start = null;
+        this.isNow = true;
+    }
+
+    private DateValue getDate(DateValue date, NumberValue diff) {
+        Calendar cal = Calendar.getInstance();
+        if (date.year != -1)
+            cal.set(Calendar.YEAR, date.year);
+        if (date.month != -1)
+            cal.set(Calendar.MONTH, date.month - 1); // Calendar month starts from 0
+        if (date.day != -1)
+            cal.set(Calendar.DATE, date.day);
+        if (date.hour != -1)
+            cal.set(Calendar.HOUR_OF_DAY, date.hour);
+        if (date.minute != -1)
+            cal.set(Calendar.MINUTE, date.minute);
+        if (date.second != -1) {
+            cal.set(Calendar.SECOND, (int) date.second);
+            cal.set(Calendar.MILLISECOND, (int) ((date.second % 1) * 1000));
+        }
+
+        switch(diff.unit) {
+            case "year":
+                cal.add(Calendar.YEAR, (int) diff.value);
+                break;
+            case "month":
+                cal.add(Calendar.MONTH, (int) diff.value);
+                break;
+            case "week":
+                cal.add(Calendar.WEEK_OF_YEAR, (int) diff.value);
+                break;
+            case "day":
+                cal.add(Calendar.DATE, (int) diff.value);
+                break;
+            case "h":
+                cal.add(Calendar.HOUR, (int) diff.value);
+                break;
+            case "s":
+                cal.add(Calendar.SECOND, (int) diff.value);
+                break;
+            default:
+                throw new RuntimeException("Invalid unit for date");
+        }
+        return new DateValue(
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH) + 1,
+                cal.get(Calendar.DATE),
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                cal.get(Calendar.SECOND) + 0.001 * cal.get(Calendar.MILLISECOND)
+                );
     }
 
     @Override
     public LispTree toLispTree() {
-        return null;
+        LispTree tree = LispTree.proto.newList();
+        tree.addChild("duration");
+        tree.addChild(this.start.toLispTree());
+        tree.addChild(this.end.toLispTree());
+        return tree;
     }
 
     @Override
     public Map<String, Object> toJson() {
-        return null;
+        Map<String, Object> json= new HashMap<>();
+        Map<String, Object> start= new HashMap<>();
+        Map<String, Object> end = new HashMap<>();
+        start.put("type", "tt.time.date");
+        end.put("type", "tt.time.date");
+        if (this.isNow)
+            end.put("value", "tt.time.now");
+        else
+            end.put("value", this.end.toJson());
+
+        if (this.start == null) {
+            start.put("value", end.get("value"));
+            start.put("diff", this.diff.toJson());
+        } else {
+            start.put("value", this.start.toJson());
+        }
+        json.put("type", "tt.time.duration");
+        json.put("from", start);
+        json.put("to", end);
+        return json;
     }
 
     @Override
@@ -39,5 +132,10 @@ public class DurationValue extends Value {
     @Override
     public int hashCode() {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return start.toString() + end.toString();
     }
 }
--- src/edu/stanford/nlp/sempre/ibase/iBase.java
+++ src/edu/stanford/nlp/sempre/ibase/iBase.java
@@ -6,23 +6,37 @@ import java.util.Map;
 import java.util.Calendar;
 
 import edu.stanford.nlp.sempre.*;
+import edu.stanford.nlp.sempre.thingtalk.*;
 
 /**
  * Created by silei on 6/9/17.
- * Value for time duration
+ * Functions for supporting ibase
+ *
  */
 public final class iBase {
-    public static DurationValue durationBetween(DateValue start, DateValue end) {
-        DurationValue duration = new DurationValue(start, end);
-        return duration;
+    public static DurationValue measureDurationCast(DateValue start, DateValue end) {
+        return new DurationValue(start, end);
     }
 
-    public static DurationValue durationBefore(DateValue start) {
-        return durationBetween(start, DateValue.now());
+    public static DurationValue measureDurationCast(DateValue start) {
+        return new DurationValue(start, DateValue.now());
     }
 
-    public static DurationValue duractionAfter(DateValue end) {
-        return durationBetween(DateValue.now(), end);
+    public static DurationValue measureDurationCast(NumberValue duration) {
+        return new DurationValue(duration);
+    }
+
+    public static Value jsonOutFilters(Value val) {
+        // e.g. {"date": {"$lt": {"type":"tt.time", "value":{"relative": "tt.time.now"}}}}
+        Map<String, Object> json = new HashMap<>();
+        json.put("date", val.toJson());
+        return (new StringValue(Json.writeValueAsStringHard(json)));
+    }
+
+    public static Value jsonOutFields() {
+        // eg. {weight: 1, _id: 0}
+        Map<String, Object> json = new HashMap<>();
+        return (new StringValue(Json.writeValueAsStringHard(json)));
     }
 
 }
